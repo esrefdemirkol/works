27445eebe01a03738403ce431a26b43a5dcfbf31
esrefdemirkol <esrefdmrkl@hotmail.com>
2020-01-08 23:14:45 +0300

    added test

---
+++ src/test/test/java/com/dimag/matematik/median/MedianCalculatorTest.java
@@ -0,0 +1,71 @@
+package com.dimag.matematik.median;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MedianCalculatorTest {
+
+    @Test
+    public void main() {
+        MedianCalculator medianCalculator = new MedianCalculator();
+        List<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(6);        //-222,1,2,5,6,226,227,228
+        list.add(1);
+        list.add(2);
+        list.add(-222);
+        list.add(226);
+        list.add(227);
+        list.add(229);
+
+        int sonuc = (int) medianCalculator.median(MedianCalculator.sort(list));
+        assertEquals(5.5,sonuc,100);
+
+    }
+
+    @Test
+    public void sort() {
+        MedianCalculator medianCalculator = new MedianCalculator();
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(5);
+        list.add(8);
+        list.add(10);
+        list.add(267);
+        list.add(14);
+        list.add(30);
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(1);
+        list2.add(5);
+        list2.add(8);
+        list2.add(10);
+        list2.add(14);
+        list2.add(30);
+        list2.add(267);
+
+
+        List<Integer> sonuc= medianCalculator.sort(list);
+        assertEquals(list2,sonuc);
+    }
+
+    @Test
+    public void median() {
+        MedianCalculator medianCalculator = new MedianCalculator();
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(5);
+        list.add(8);
+        list.add(10);
+        list.add(267);
+        list.add(14);
+        list.add(30);
+        double sonuc = medianCalculator.median(list);
+        assertEquals(10,sonuc,1);
+
+    }
+}
